e4c267874132a744d030d9d0a2a5ffa95fb5fea8
samm <muiruri.samwel@outlook.com>
2023-02-27 19:27:06 +0300

    url fix 2

--- resources/views/base/navbar.blade.php
+++ resources/views/base/navbar.blade.php
@@ -2,18 +2,19 @@
     <div class="container-fluid h-100">
         <div class="row no-gutters justify-content-center align-items-center h-100">
             <div class="col-md-4">
+                <!-- Note for live server avoid using the url typo - only use it in local + dev env -->
                 <div class="navigation_inner">
                     <div class="nav__label">
-                        <a href="{{ url ('/home')}}" class="nav__label_inner slide-horizontal hbrable" target="_self" data-splitting>Home</a>
+                        <a href="/home" class="nav__label_inner slide-horizontal hbrable" target="_self" data-splitting>Home</a>
                     </div>
                     <div class="nav__label">
-                        <a href="{{ url ('/work')}}" class="nav__label_inner slide-horizontal hbrable" target="_self" data-splitting>Work</a>
+                        <a href="/work" class="nav__label_inner slide-horizontal hbrable" target="_self" data-splitting>Work</a>
                     </div>
                     <div class="nav__label">
-                        <a href="{{ url ('/about')}}" class="nav__label_inner slide-horizontal hbrable" target="_self"  data-splitting>About</a>
+                        <a href="/about" class="nav__label_inner slide-horizontal hbrable" target="_self"  data-splitting>About</a>
                     </div>
                     <div class="nav__label">
-                        <a href="{{ url ('/contact')}}" class="nav__label_inner slide-horizontal hbrable " rel="noopener" target="_self" data-splitting>Contact</a>
+                        <a href="/contact" class="nav__label_inner slide-horizontal hbrable " rel="noopener" target="_self" data-splitting>Contact</a>
                     </div>
                 </div>
             </div>
--- resources/views/pages/work.blade.php
+++ resources/views/pages/work.blade.php
@@ -24,32 +24,33 @@
             <!-- start works wrapper -->
             <div class="wrapper works__wrapper">
                 <div class="container">
+                     <!-- Note for live server avoid using the url typo - only use it in local + dev env -->
                     <div class="row justify-content-center">
                         <div class="col-lg-8 col-md-12">
                             <!-- work-item -->
                             <div class="works__item">
-                                <h2 class="work__title"> <span class="project__count">01</span> <a class="ajax-popup-link hbrable" href="{{ url ('/headphones')}}">BɚZE</a> </h2>
+                                <h2 class="work__title"> <span class="project__count">01</span> <a class="ajax-popup-link hbrable" href="/headphones">BɚZE</a> </h2>
                             </div>
                            <!-- work-item -->
                             <div class="works__item_0">
-                                <h2 class="work__title_0"> <span class="project__count_0">02</span> <a class="ajax-popup-link hbrable" href="{{ url ('/table')}}">Glæss</a> </h2>
+                                <h2 class="work__title_0"> <span class="project__count_0">02</span> <a class="ajax-popup-link hbrable" href="/table">Glæss</a> </h2>
                             </div>
                              <!-- work-item -->
                            <div class="works__item_1">
-                                <h2 class="work__title_1"> <span class="project__count_1">03</span> <a class="ajax-popup-link hbrable" href="{{ url ('/blog')}}">Not.ed</a></h2>
+                                <h2 class="work__title_1"> <span class="project__count_1">03</span> <a class="ajax-popup-link hbrable" href="/blog">Not.ed</a></h2>
                             </div>
-                             {{--<!-- work-item -->
+                            <!-- work-item 
                            <div class="works__item">
                                 <h2 class="work__title"> <span class="project__count">04</span> <a class="ajax-popup-link hbrable" href="">Roadtrip</a> </h2>
                             </div>
-                            <!-- work-item -->
+                            <!-- work-item 
                             <div class="works__item">
                                 <h2 class="work__title"> <span class="project__count">05</span> <a class="ajax-popup-link hbrable" href="">Unit-Pen</a> </h2>
                             </div>
-                            <!-- work-item -->
+                            <!-- work-item 
                             <div class="works__item">
                                 <h2 class="work__title"> <span class="project__count">06</span> <a class="ajax-popup-link hbrable" href="">City Lights</a> </h2>
-                            </div> --}}
+                            </div> -->
                         </div>
                     </div>
                 </div>
