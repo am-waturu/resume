cd2e8f12c96726e5c310060ac03ac8833c9249a2
samm <muiruri.samwel@outlook.com>
2023-02-27 20:14:57 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Contracts\Routing\UrlGenerator;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,11 +17,15 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-      public function boot(UrlGenerator $url)
+    public function boot(): void
     {
-        if(env('APP_ENV') !== 'local')
-        {
-            $url->forceScheme('https');
-        }
+        //
     }
+    //    public function boot(UrlGenerator $url)
+    // {
+    //     if(env('APP_ENV') !== 'local')
+    //     {
+    //         $url->forceScheme('https');
+    //     }
+    // }
 }
